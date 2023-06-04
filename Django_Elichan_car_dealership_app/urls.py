5256b65ec3693b9edc90703bc2cd5ccaca6b1121
Christian Dela Gente <xchan344@gmail.com>
2023-06-04 22:33:58 +0800

    URL update

--- Django_Elichan_car_dealership_app/urls.py
+++ Django_Elichan_car_dealership_app/urls.py
@@ -8,6 +8,7 @@ app_name = 'employees'
 
 urlpatterns = [
     #login
+    path('', views.login_view, name='login'),
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
 
